9e61269c6e27e6f25d02ce7ebf5edaa43888df22
fernandofckc-del <fernando.fckc@gmail.com>
2026-09-06 17:07:21 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Como o app é um único arquivo HTML (sem backend), o cache aqui é bem
    simples: guarda o próprio app para abrir mesmo sem internet. */
 
-const CACHE = 'horta-v31';
+const CACHE = 'horta-v32';
 const ARQUIVOS = ['./index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png', './fundo-inicio.jpg'];
 
 self.addEventListener('install', (event) => {
